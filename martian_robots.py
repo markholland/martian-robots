04f0c550850b3c5571a919a5b30964c22396ec5f
Mark Holland <mark@partiallogic.com>
2017-10-17 18:54:59 +0100

    adds outline of main program

---
+++ martian_robots.py
@@ -0,0 +1,21 @@
+"""Martian Robots program"""
+def main():
+    """
+        This is a program for calculating the position of a robot on Mars
+        after following a series of instructions
+    """
+
+    # input validation
+    # validate grid size
+    # loop robots
+        # validate starting position
+        # validate starting orientation
+        # validate instruction length
+        # validate instruction content
+        # calc final position
+            # mark scent
+            # detect scent
+        # print output
+            # LOST if left grid
+    # end loop
+    # end
