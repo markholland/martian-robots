2331f7d9679a1b6b391bf1c5154cb64180379415
Mark Holland <mark@partiallogic.com>
2017-10-17 18:47:56 +0100

    adds test outline

---
+++ tests.py
@@ -0,0 +1,46 @@
+""" Unit tests for Martian Robots program"""
+import unittest
+
+class TestInputValidation(unittest.TestCase):
+    def setUp(self):
+        assert False
+
+    def test_validate_grid_size(self):
+        assert False
+
+    def test_validate_starting_position(self):
+        assert False
+
+    def test_validate_starting_orientation(self):
+        assert False
+
+    def test_validate_instruction_length(self):
+        assert False
+
+    def test_validate_instruction(self):
+        assert False
+
+    def tearDown(self):
+        assert False
+
+class TestMartianRobots(unittest.TestCase):
+    def setUp(self):
+        assert False
+
+    def test_change_position(self):
+        assert False
+
+    def test_leave_scent(self):
+        assert False
+
+    def test_detect_scent_and_continue(self):
+        assert False
+
+    def test_ignore_other_robots(self):
+        assert False
+
+    def tearDown(self):
+        assert False
+
+if __name__ == '__main__':
+    unittest.main()
