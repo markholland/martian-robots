d906ae8483875a24c8cfcb6f9087771141c59246
Mark Holland <mark@partiallogic.com>
2017-10-17 19:37:24 +0100

    validates grid size input

--- martian_robots.py
+++ martian_robots.py
@@ -1,9 +1,18 @@
 """Martian Robots program"""
+
+import sys
+
 def main():
     """
         This is a program for calculating the position of a robot on Mars
         after following a series of instructions
     """
+
+    grid_size_input = input('Enter grid size: ')
+    if(not valid_grid_size(grid_size_input)):
+        print "Grid size should be a pair of coordinates between 0 0 & 50 50"
+        sys.exit()
+    grid_size = grid_size_input.split()
 
     # input validation
     # validate grid size
@@ -19,3 +28,15 @@ def main():
             # LOST if left grid
     # end loop
     # end
+    
+def valid_grid_size(grid_size):
+    grid_size = grid_size.split()
+    if len(grid_size) != 2:
+        return False
+    for coordinate in grid_size:
+        if not coordinate.isdigit():
+            return False
+        if int(coordinate) > 50:
+            return False
+    return True
+
--- tests.py
+++ tests.py
@@ -1,46 +1,73 @@
 """ Unit tests for Martian Robots program"""
 import unittest
 
+from martian_robots import valid_grid_size
+
 class TestInputValidation(unittest.TestCase):
     def setUp(self):
-        assert False
+        pass
 
     def test_validate_grid_size(self):
-        assert False
+        grid_size = '1 2'
+        self.assertTrue(valid_grid_size(grid_size))
 
-    def test_validate_starting_position(self):
-        assert False
+        grid_size = '50 50'
+        self.assertTrue(valid_grid_size(grid_size))
 
-    def test_validate_starting_orientation(self):
-        assert False
+        grid_size = '50 51'
+        self.assertFalse(valid_grid_size(grid_size))
 
-    def test_validate_instruction_length(self):
-        assert False
+        grid_size = '51 50'
+        self.assertFalse(valid_grid_size(grid_size))
 
-    def test_validate_instruction(self):
-        assert False
+        grid_size = '0 -1'
+        self.assertFalse(valid_grid_size(grid_size))
 
-    def tearDown(self):
-        assert False
+        grid_size = '1'
+        self.assertFalse(valid_grid_size(grid_size))
 
-class TestMartianRobots(unittest.TestCase):
-    def setUp(self):
-        assert False
+        grid_size = '1 2 3'
+        self.assertFalse(valid_grid_size(grid_size))
 
-    def test_change_position(self):
-        assert False
+        grid_size = 'a'
+        self.assertFalse(valid_grid_size(grid_size))
 
-    def test_leave_scent(self):
-        assert False
+        grid_size = '1,3'
+        self.assertFalse(valid_grid_size(grid_size))
 
-    def test_detect_scent_and_continue(self):
-        assert False
+#     def test_validate_starting_position(self):
+#         assert False
 
-    def test_ignore_other_robots(self):
-        assert False
+#     def test_validate_starting_orientation(self):
+#         assert False
 
-    def tearDown(self):
-        assert False
+#     def test_validate_instruction_length(self):
+#         assert False
+
+#     def test_validate_instruction(self):
+#         assert False
+
+#     def tearDown(self):
+#         assert False
+
+# class TestMartianRobots(unittest.TestCase):
+#     def setUp(self):
+#         assert False
+
+#     def test_change_position(self):
+#         assert False
+
+#     def test_leave_scent(self):
+#         assert False
+
+#     def test_detect_scent_and_continue(self):
+#         assert False
+
+#     def test_ignore_other_robots(self):
+#         assert False
+
+#     def tearDown(self):
+#         assert False
 
 if __name__ == '__main__':
     unittest.main()
